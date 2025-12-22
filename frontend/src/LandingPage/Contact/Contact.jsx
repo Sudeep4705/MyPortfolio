5ebda6d140ba4f968a90a677f8d14c8f9428b769
Sudeep <sudeepsudee828@gmail.com>
2025-12-22 11:44:52 +0530

    new project added

--- frontend/src/LandingPage/Contact/Contact.jsx
+++ frontend/src/LandingPage/Contact/Contact.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 const contactInfo = [
   {
     method: 'Email',
-    value: 'sudeepsudee828@gmail.com',
+    value: 'sudeep.mern@gmail.com',
 
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-8 w-8">
